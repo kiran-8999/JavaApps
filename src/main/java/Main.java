8b0c041fcf97c094311d55b02998a201f56687cb
kiran-8999 <mahalekiran6205@gmail.com>
2023-02-01 23:52:52 +0530

    Adding my project to repository

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkIfInputIsEvenNumber(122));
+    }
+
+    public static boolean checkIfInputIsEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestMain {
+
+    @Test
+    public void testIsEven(){
+
+        assertTrue(Main.checkIfInputIsEvenNumber(24));
+
+    }
+}
